13b600688f30735e5ddd15840a1c8b1f358fbe79
David Layne <davidlayne@usf.edu>
2024-10-20 22:12:30 -0400

    Handle stock availability and disable purchase button when out of stock // Create checkout event and confirmation message

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+//2. Add event listeners for product selection
 const prices = document.getElementById('price');
 const sizes = document.getElementById('size');
+const button = document.getElementById('purchase');
 
-sizes.addEventListener('change', function(event) {
-    const selectedPrice = event.target.value;
-    prices.textContent = `$${selectedPrice}`;
+
+sizes.addEventListener('change', (event) => {
+    const selectedData = event.target.options[event.target.selectedIndex];
+    const price = selectedData.getAttribute('value');
+    prices.textContent = `$${price}`;
+    const stock = selectedData.getAttribute('data-Availability');
+
+//3. Handle Stock Availability
+    if (stock === 'out-of-stock'){
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+});
+
+//4. Checkout Event
+button.addEventListener('click', () => {
+
+    const selectedData = sizes.options[sizes.selectedIndex];
+    const stock = selectedData.getAttribute('data-availability');
+
+    if (stock === 'in-stock'){
+        alert('Purchase Complete!');
+    } else {
+        alert('Product out of stock and cannot be purchased.');
+    }
+
 });
